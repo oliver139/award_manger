0cbfd924797f1bad19cd7d55ceac499a00c0f0f1
Oliver Mak <oliver139.working@gmail.com>
2022-04-06 00:32:47 +0800

    Provide form for associating feature

--- award_img_manager.php
+++ award_img_manager.php
@@ -44,6 +44,7 @@ class Award_Img_Manager extends Module
         return parent::install() &&
             $this->createImageDir() &&
             $this->hooksRegistration() &&
+            $this->setConfigValues() &&
             $this->installDB() &&
             $this->addTab();
     }
@@ -51,7 +52,8 @@ class Award_Img_Manager extends Module
     public function uninstall() {
         return parent::uninstall() && 
             $this->uninstallDB() &&
-            $this->removeTab();
+            $this->removeTab() &&
+            $this->removeConfigValues();
     }
 
     /**
@@ -94,6 +96,34 @@ class Award_Img_Manager extends Module
         ];
 
         return $this->registerHook($hooks);
+    }
+
+    public function setConfigValues() {
+        $res = true;
+
+        $config = [
+            'AWARD_IMG_FEATURE_ID' => '1',
+        ];
+
+        foreach ($config as $key => $value) {
+            $res &= Configuration::updateValue($key, $value);
+        }
+        
+        return $res;
+    }
+
+    public function removeConfigValues() {
+        $res = true;
+
+        $config = [
+            'AWARD_IMG_FEATURE_ID',
+        ];
+
+        foreach ($config as $value) {
+            $res &= Configuration::deleteByName($value);
+        }
+        
+        return $res;
     }
 
     private function createImageDir() {
@@ -162,5 +192,115 @@ class Award_Img_Manager extends Module
             }
         }
         return true;
+    }
+
+    public function renderForm() {
+        $feature_radio = [];
+        $features = Feature::getFeatures(1);
+        foreach ($features as $f) {
+            $feature_radio[] = [
+                'id' => $f['id_feature'],
+                'value' => $f['id_feature'],
+                'label' => '[' . $f['id_feature'] . '] ' . $f['name'],
+            ];
+        }
+
+        $fields_form[0]['form'] = [
+            'legend' => [
+                'title' => $this->l('Setting'),
+                'icon' => 'icon-cogs',
+            ],
+            'input' => [
+                [
+                    'type' => 'radio',
+                    'label' => $this->l('Link to which feature'),
+                    'name' => 'award_fid',
+                    'values' => $feature_radio,
+                    'required' => true,
+                ],
+            ],
+            'submit' => [
+                'title' => $this->l('Save'),
+                'class' => 'btn btn-default pull-right'
+            ],
+        ];
+
+        // load helperForm
+        $helper = new HelperForm();
+		$helper->show_cancel_button = true;
+
+        // module, token and currentIndex
+        $helper->module = $this;
+        $helper->name_controller = $this->name;
+        $helper->token = Tools::getAdminTokenLite('AdminModules');
+        $helper->currentIndex = AdminController::$currentIndex.'&configure='.$this->name;
+
+        // title and toolbar
+        $helper->title = $this->displayName;
+        $helper->show_toolbar = true;        // false -> remove toolbar
+        $helper->toolbar_scroll = true;      // yes - > Toolbar is always visible on the top of the screen.
+        $helper->submit_action = 'submit'.$this->name;
+        $helper->toolbar_btn = array(
+            'save' =>
+                array(
+                    'desc' => $this->l('Save'),
+                    'href' => AdminController::$currentIndex.'&configure='.$this->name.'&save'.$this->name.
+                        '&token='.Tools::getAdminTokenLite('AdminModules'),
+                ),
+            'back' => array(
+                'href' => AdminController::$currentIndex.'&token='.Tools::getAdminTokenLite('AdminModules'),
+                'desc' => $this->l('Back to list')
+            )
+        );
+
+        // Language
+        foreach (Language::getLanguages(false) as $lang){
+            $helper->languages[] = [
+                'id_lang' => $lang['id_lang'],
+                'iso_code' => $lang['iso_code'],
+                'name' => $lang['name'],
+                'is_default' => ($this->context->language->id == $lang['id_lang'] ? 1 : 0)
+            ];
+        }
+        $helper->default_form_language = $this->context->language->id;
+        $helper->allow_employee_form_lang = Configuration::get('PS_BO_ALLOW_EMPLOYEE_FORM_LANG', 0);
+
+        // load current value
+        $helper->tpl_vars = [
+            'fields_value' => $this->getConfigFieldsValues(),
+            'languages' => $this->context->controller->getLanguages(),
+            'id_language' => $this->context->language->id,
+        ];
+
+
+        // CSS & JS
+        $this->context->controller->addCSS($this->_path.'assets/css/genki_info.css');
+        // $this->context->controller->addJS($this->_path.'views/js/form.js');
+
+        return $helper->generateForm($fields_form);
+    }
+
+    public function getConfigFieldsValues() {
+        $fields = [
+            'award_fid' => Configuration::get('AWARD_IMG_FEATURE_ID'),
+        ];
+
+        return $fields;
+    }
+
+    public function getContent() {
+        $output = '';
+
+        // here we check if the form is submited for this module
+        if (Tools::isSubmit('submit'.$this->name)) {
+            $config['AWARD_IMG_FEATURE_ID'] = Tools::getValue('award_fid');
+
+            foreach ($config as $key => $value){
+                Configuration::updateValue($key, $value);
+            }
+
+            $output .= $this->displayConfirmation($this->l('Setting Updated!'));
+        }
+        return $output.$this->renderForm();
     }
 }
